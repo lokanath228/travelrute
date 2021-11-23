bdd4e15f23b85a24bd34f68bf99110094f241255
lokanath <loguvima228@gmail.com>
2021-11-23 17:18:57 +0530

    first commit

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,15 +1,7 @@
 import React from "react";
 import '../../App.css'
 
-function app () {
-   return( 
-   <div>
-       <h1>here submit ur service details</h1>
-    <textarea>type here</textarea>
-     </div>
-  
-   )
-}
+
 
 export default function Services() {
     return <h1 className='services'>SERVICES</h1>
